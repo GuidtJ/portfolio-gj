90e0a3ea64a637ca4ce20a216d71b702a4390523
GuidtJ <jeromeguidt@hotmail.fr>
2024-05-21 16:01:17 +0200

    modification de a propos

--- js/script.js
+++ js/script.js
@@ -1,77 +1,76 @@
 // *============================= toggle icon navbar =======================*//
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
 };
-
-
 
 // *============================= scroll sections active link =======================*//
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
-    sections.forEach(sec =>{
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 150;
-        let height = sec.offsetHeight;
-        let id =sec.getAttribute('id');
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
 
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id +']').classList.add('active');
-            });
-        };
-    });
-// *============================= sticky navbar =======================*//
-    let header = document.querySelector('header');
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+  // *============================= sticky navbar =======================*//
+  let header = document.querySelector("header");
 
-    header.classList.toggle('sticky', window.scrollY > 100 );
+  header.classList.toggle("sticky", window.scrollY > 100);
 
-    // *============================= remove toggle icon navbar when click navbar link (scroll) =======================*//
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
+  // *============================= remove toggle icon navbar when click navbar link (scroll) =======================*//
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 };
 
-
 // *============================= scroll reveal =======================*//
-ScrollReveal({ 
-    // reset: true,
-    distance: '80px',
-    duration: 2000,
-    delay: 200
-
+ScrollReveal({
+  // reset: true,
+  distance: "80px",
+  duration: 2000,
+  delay: 200,
 });
 
-ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
-ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+ScrollReveal().reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .portfolio-box, .contact form",
+  { origin: "bottom" }
+);
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
 
 // *============================= typed js =======================*//
-const typed = new Typed('.multiple-text', {
-    strings: ['Développeur Web', 'Développeur Web', 'Développeur Web'],
-    typeSpeed: 100,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true
-
+const typed = new Typed(".multiple-text", {
+  strings: ["Développeur Web", "Développeur Web", "Développeur Web"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
 });
 
 // ---------------------------Timer--------------------------------//
 function heure() {
-    var date = new Date(); 
-    var heures = date.getHours();
-    var minutes = date.getMinutes();
+  var date = new Date();
+  var heures = date.getHours();
+  var minutes = date.getMinutes();
 
-    heures = heures < 10 ? "0" + heures : heures;
-    minutes = minutes < 10 ? "0" + minutes : minutes;
+  heures = heures < 10 ? "0" + heures : heures;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
 
-    document.getElementById("horloge").innerHTML =  + heures + ":" + minutes;
-
-  }
-  setInterval(heure, 1000);
+  document.getElementById("horloge").innerHTML = +heures + ":" + minutes;
+}
+setInterval(heure, 1000);
